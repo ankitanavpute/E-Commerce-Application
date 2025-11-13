b5d12a04d54730fb682923bd5ad3ffecc40eed31
Ankita Dnyaneshwar Navpute <103128674+ankitanavpute@users.noreply.github.com>
2025-11-13 10:05:37 +0530

    Add files via upload

---
+++ AspNetFirstWebApi/Controllers/FruitsApiController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetFirstWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitsApiController : ControllerBase
+    {
+        public List<string> fruits = new List<string>()
+        {
+            "Apple",
+            "Banana",
+            "Mango",
+            "Cherry",
+            "Grapes"
+        };
+        [HttpGet]
+        public List<string> GetFruits() 
+        {
+            return fruits;
+
+
+
+
+        }
+    }
+}
